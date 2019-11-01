1877bd934b62260f6ecdb10001a3cea466b83cf2
Bek <btnkb@mail.umsl.edu>
2019-11-01 15:23:02 -0500

    Added explanation on README file about how prompt_user_for_rules need will work
    and extra validations to actual code

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # JalapenoHotties
 CS 4500 big project - Listener with Raspberry Pi
+
+
+*******     prompt_user_for_rules.py     ************
+
+It will ask user to input the words, phrases or patterns they want to look for.
+- First program will start prompting user to input words they want to count separated by comma
+    Example: sine, cosine, tangent, cotangent
+  After accepting words, program will parse it by comma and validate that it does not exceed the maximum number of words
+  We also have auto correct which will identify possible misspelled words and ask user if that word was misspelled, so they
+  can change the word or keep it the way it is.
+  
+- Second program will prompt user to input phrases they want to count separated by comma
+    Example: cosine and tangent, derivative of sine and so on
+  After accepting phrases, program will parse it by comma and validate that it does not exceed the maximum number of words
+  We also have auto correct which will identify possible misspelled words and ask user if that word was misspelled, so they
+  can change the word or keep it the way it is.
+  
+- Third program will prompt user to input patterns they want to count separated by comma
+    Example: cosine sine 2 15, derivative tangent 5 13
+  After accepting patterns, program will parse it by comma and validate that it does not exceed the maximum number of words
+  We also have auto correct which will identify possible misspelled words and ask user if that word was misspelled, so they
+  can change the word or keep it the way it is.
+  For lower bound and upper bound numbers it validates if input is actually a number or not.
+  
+After gathering all the data program will output to a file called "rules.txt"
--- prompt_user_for_rules.py
+++ prompt_user_for_rules.py
@@ -15,15 +15,37 @@ def validate(input_data, max_input):
 
 def parse_message(input_data, option):
     trimmed_array = []
+    temp_array = []
+    temp_string_split = []
 
     if option == 1:
-        if len(input_data.split(" ")) != len(input_data.split(",")):
+        if len(input_data.split(" ")) != len(input_data.split(",")):     # Check if number of words is same when parsed
             return False
+    if option == 3:
+        temp_array = input_data.split(",")      # First parse it by comma
+        for element in temp_array:
+            temp_trimmer = []
+            temp_string_split = (element.strip()).split(" ")     # Second parse it by space
+            print(temp_string_split)
+            if len(temp_string_split) == 4:     # Make sure it has 4 elements
+                count = 1
+                for value in temp_string_split:     # Check for first two words autocorrect and than 3rd and 4th integer
+                    if count == 1 or count == 2:
+                        temp_trimmer.append(spell.correction(value.strip()))
+                    if count == 3 or count == 4:
+                        if value.isdigit():     # Is it integer?
+                            temp_trimmer.append(value)
+                        else:
+                            return False
+                    count += 1
+            else:
+                return False
+            trimmed_array.append(temp_trimmer)
+        return trimmed_array
 
     data_array = input_data.split(",")
     for x in data_array:
         trimmed_array.append(spell.correction(x.strip()))
-    print(trimmed_array)
     return trimmed_array
 
 
@@ -38,7 +60,6 @@ def display_message(output_message):
 
 def main():
     user_input = ""     # Will get modified overtime can be list or just string
-    separator = ","
     quit_prompt = False
     MAX_WORDS = 6
     MAX_PHRASES = 4
@@ -61,7 +82,7 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
-            pass    # Eventually output that to file
+            print(user_input)    # Eventually output that to file
 
     quit_prompt = False     # reset prompter
     while not quit_prompt:
@@ -73,11 +94,11 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
-            pass    # Eventually output that to file
+            print(user_input)    # Eventually output that to file
 
     quit_prompt = False  # reset prompter
     while not quit_prompt:
-        display_message(PROMPT_FOR_PHRASES)
+        display_message(PROMPT_FOR_PATTERN)
         user_input = read_input()
         user_input = parse_message(user_input, PROMPT_PATTER_OPTION)
         quit_prompt = validate(user_input, MAX_PATTERNS)
@@ -85,7 +106,7 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
-            pass  # Eventually output that to file
+            print(user_input)  # Eventually output that to file
 
 
 main()
