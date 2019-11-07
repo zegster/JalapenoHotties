a7e0e531e4d1423bb46cb38d3de4d3281e47ee11
Bek <btnkb@mail.umsl.edu>
2019-11-06 18:01:33 -0600

    added some validations

--- prompt_user_for_rules.py
+++ prompt_user_for_rules.py
@@ -1,3 +1,4 @@
+# type following to terminal to install: pip install pyspellchecker
 from spellchecker import SpellChecker
 # Checks the spelling
 spell = SpellChecker()
@@ -5,9 +6,11 @@ spell = SpellChecker()
 
 def validate(input_data, max_input):
     if not input_data:
+        print("input size wrong")
         return False
 
     if len(input_data) > max_input:
+        print("input size wrong")
         return False
 
     return True
@@ -21,8 +24,74 @@ def parse_message(input_data, option):
     if option == 1:
         if len(input_data.split(" ")) != len(input_data.split(",")):     # Check if number of words is same when parsed
             return False
+        else:
+            data_array = input_data.split(",")
+            corrected_input = ""
+            choice_option = ""
+            for x in data_array:
+                if spell.correction(x.strip()) != x.strip():
+                    print("Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nDo you "
+                          "wish to change it:\n1) Yes\n2) No" % (x.strip(), spell.correction(x.strip())))
+                    choice_option = input()
+                    while int(choice_option) != 1 and int(choice_option) != 2:
+                        print(
+                            "Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nDo you "
+                            "wish to change it:\n1) Yes\n2) No" % (x.strip(), spell.correction(x.strip())))
+                        choice_option = input()
+                    print(choice_option)
+                    if int(choice_option) == 1:
+                        print(
+                            "Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nplease input corrected word: " % (
+                                x.strip(), spell.correction(x.strip())))
+                        corrected_input = input()
+                        while spell.correction(corrected_input.strip()) != corrected_input:
+                            print(
+                                "Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nplease input corrected word: " % (
+                                corrected_input, spell.correction(corrected_input.strip())))
+                            corrected_input = input()
+                        trimmed_array.append(corrected_input.strip())
+                    else:
+                        trimmed_array.append(x.strip())
+                else:
+                    trimmed_array.append(x.strip())
+            return trimmed_array
+    if option == 2:
+        data_array = input_data.split(",")
+        corrected_input = ""
+        choice_option = ""
+        temp_trimmed_array = []
+        for y in data_array:
+            split_by_space = y.split(" ")
+            for x in split_by_space:
+                if spell.correction(x.strip()) != x.strip():
+                    print("Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nDo you "
+                          "wish to change it:\n1) Yes\n2) No" % (x.strip(), spell.correction(x.strip())))
+                    choice_option = input()
+                    while int(choice_option) != 1 and int(choice_option) != 2:
+                        print("Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nDo you "
+                              "wish to change it:\n1) Yes\n2) No" % (x.strip(), spell.correction(x.strip())))
+                        choice_option = input()
+                    print(choice_option)
+                    if int(choice_option) == 1:
+                        print(
+                            "Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nplease input corrected word: " % (
+                                x.strip(), spell.correction(x.strip())))
+                        corrected_input = input()
+                        while spell.correction(corrected_input.strip()) != corrected_input:
+                            print(
+                                "Is the given word spelled correctly?\nGiven input: (%s) \nPossible spelling: (%s)\nplease input corrected word: " % (
+                                corrected_input, spell.correction(corrected_input.strip())))
+                            corrected_input = input()
+                        temp_trimmed_array.append(corrected_input.strip())
+                    else:
+                        temp_trimmed_array.append(y.strip())
+                else:
+                    temp_trimmed_array.append(y.strip())
+            trimmed_array.append(temp_trimmed_array)
+            return trimmed_array
     if option == 3:
         temp_array = input_data.split(",")      # First parse it by comma
+        temp_value = 0
         for element in temp_array:
             temp_trimmer = []
             temp_string_split = (element.strip()).split(" ")     # Second parse it by space
@@ -32,8 +101,18 @@ def parse_message(input_data, option):
                 for value in temp_string_split:     # Check for first two words autocorrect and than 3rd and 4th integer
                     if count == 1 or count == 2:
                         temp_trimmer.append(spell.correction(value.strip()))
-                    if count == 3 or count == 4:
+                    if count == 3:
                         if value.isdigit():     # Is it integer?
+                            if int(value) < 2 or int(value) > 15:
+                                return False
+                            temp_trimmer.append(value)
+                            temp_value = int(value)
+                        else:
+                            return False
+                    if count == 4:
+                        if value.isdigit():     # Is it integer?
+                            if temp_value > int(value) or int(value) > 15:
+                                return False
                             temp_trimmer.append(value)
                         else:
                             return False
@@ -43,10 +122,7 @@ def parse_message(input_data, option):
             trimmed_array.append(temp_trimmer)
         return trimmed_array
 
-    data_array = input_data.split(",")
-    for x in data_array:
-        trimmed_array.append(spell.correction(x.strip()))
-    return trimmed_array
+
 
 
 def read_input():
@@ -60,6 +136,7 @@ def display_message(output_message):
 
 def main():
     user_input = ""     # Will get modified overtime can be list or just string
+    output_file_name = "rules.txt"
     quit_prompt = False
     MAX_WORDS = 6
     MAX_PHRASES = 4
@@ -67,7 +144,7 @@ def main():
     PROMPT_FOR_WORDS = "Please input up to 6 words separated by comma',' (word_1, word_2...):"
     PROMPT_FOR_PHRASES = "Please input up to 4 phrases separated by comma',' (phrase_1, phrase_2...):"
     PROMPT_FOR_PATTERN = "Please input up to 2 pattern followed by upper-bound=u, lower-bound=l " \
-                         "separated by comma',' (word_1 word_2 u l, word_1 word_2 u l...):"
+                         "separated by comma',' (word_1 word_2 l u, word_1 word_2 l u...):"
     ERROR_MSG = "ERROR: Given input was wrong please follow the following example:"
     PROMPT_WORD_OPTION = 1
     PROMPT_PHRASE_OPTION = 2
@@ -82,6 +159,9 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
+            f = open(output_file_name, "w")
+            for word in user_input:
+                f.write(word + "\n")
             print(user_input)    # Eventually output that to file
 
     quit_prompt = False     # reset prompter
@@ -94,6 +174,8 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
+            for word in user_input:
+                f.write(word + "\n")
             print(user_input)    # Eventually output that to file
 
     quit_prompt = False  # reset prompter
@@ -106,7 +188,13 @@ def main():
         if not quit_prompt:
             display_message(ERROR_MSG)
         else:
+            for word in user_input:
+                for x in word:
+                    f.write(x)
+                f.write("\n")
             print(user_input)  # Eventually output that to file
+
+    f.close()
 
 
 main()
